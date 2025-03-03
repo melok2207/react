fbf0750bf402c5292aa28dd52646018c42a4df0c
melok2207 <132545373+melok2207@users.noreply.github.com>
2025-03-03 18:51:44 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+ import React from "react";
+   import UserForm from "./components/UserForm/UserForm";
+
+   function App() {
+       return (
+           <div className="App">
+               <UserForm />
+           </div>
+       );
+   }
